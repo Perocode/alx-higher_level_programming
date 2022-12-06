c662cbb524cbf10a11fedc528b3f243ca55cba1b
Perocode <ogunromeo@gmail.com>
2022-12-05 20:51:09 -0800

    palindrome

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -11,17 +11,16 @@
  * @next: points to thee next node
  * Description: singly lisy node structure
  */
-
 typedef struct listint_s
 {
   int n;
-  struct listint_s * next;
+  struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
-void free_listint(l;istint_t *head);
-void reverse_listint(listint_t **head);
-int is_palindrome(listint_t **head);
+void free_listint(listint_t *head);
 
-#endif
+int is_palindrome(listint_t **head);
+void reverse_listint(listint_t **head);
+#endif /* LISTS_H */
